51a87c4d3c3b59bce9998603f747d5d15c5c191a
AnnaNordio <anna.nordio02@gmail.com>
2026-03-27 21:42:52 +0100

    feat: added Grace period

--- gateway/state.go
+++ gateway/state.go
@@ -13,4 +13,8 @@ var (
 
     currentMode = "polling"
     currentSize = "small"
+
+	warmupThreshold = 5
+    grpcCount       = 0
+    restCount       = 0
 )
--- gateway/telemetry.go
+++ gateway/telemetry.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+    "fmt"
     "time"
 )
 
@@ -11,11 +12,23 @@ func saveMetric(protocol string, sensorTimestamp int64) {
     defer metricsMu.Unlock()
 
     if protocol == "gRPC" {
+        if grpcCount < warmupThreshold {
+            grpcCount++
+            fmt.Printf("⏳ [Warm-up gRPC] %d/%d\n", grpcCount, warmupThreshold)
+            return
+        }
         lastGlobalGrpcTS = sensorTimestamp
+    } else if protocol == "REST" {
+        if restCount < warmupThreshold {
+            restCount++
+            fmt.Printf("⏳ [Warm-up REST] %d/%d\n", restCount, warmupThreshold)
+            return
+        }
     }
+    // ---------------------------
 
     now := time.Now().UnixMicro()
-    realLatency := float64(now - sensorTimestamp)
+    realLatency := float64(now-sensorTimestamp) 
     displayTS := time.UnixMicro(sensorTimestamp).Format("15:04:05.000") 
 
     history = append(history, Metric{
@@ -35,9 +48,11 @@ func savePayload(protocol string, size int) {
     defer metricsMu.Unlock()
 
     if protocol == "REST" {
+        if restCount < warmupThreshold { return }
         sumSizeRest += float64(size)
         countSizeRest++
     } else {
+        if grpcCount < warmupThreshold { return }
         sumSizeGrpc += float64(size)
         countSizeGrpc++
     }
@@ -46,5 +61,13 @@ func savePayload(protocol string, size int) {
 func resetStats() {
     metricsMu.Lock()
     defer metricsMu.Unlock()
+    
+    // Azzeriamo tutto: history, medie e contatori di warm-up
     history = []Metric{}
+    sumSizeRest = 0
+    countSizeRest = 0
+    sumSizeGrpc = 0
+    countSizeGrpc = 0
+    
+    fmt.Println("Reset completo: statistiche azzerate.")
 }
